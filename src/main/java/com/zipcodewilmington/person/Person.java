508addc70e1a139531f342ec5b518ffacefa61bb
Jay <73120876+JarrellW@users.noreply.github.com>
2020-11-06 15:31:46 -0500

    added 5 more fields

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,10 +6,22 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String hairColor;
+    private String eyeColor;
+    private int height;
+    private int weight;
+    private String skinColor;
+
+
 
     public Person() {
       this.name = "";
       this.age = Integer.MAX_VALUE;
+      this.hairColor = "";
+      this.eyeColor = "";
+      this.height = Integer.MAX_VALUE;
+      this.weight =  Integer.MAX_VALUE;
+      this.skinColor = "";
 
 
     }
@@ -34,6 +46,48 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+
+    }
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+
+    }
+    public void setHeight(int height) {
+        this.height = height;
+
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+
+    }
+    public void setSkinColor(String skinColor) {
+        this.skinColor = skinColor;
+
+    }
+
+
+    public String getHairColor() {
+        return this.hairColor;
+    }
+
+    public String getSkinColor() {
+        return this.skinColor;
+    }
+
+    public Integer getWeight() {
+        return this.weight;
+    }
+
+    public Integer getHeight() {
+        return this.height;
+    }
+
+    public String getEyeColor() {
+        return this.eyeColor;
     }
 
     public String getName() {
